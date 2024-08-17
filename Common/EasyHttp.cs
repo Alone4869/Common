94944bf32f7b41d73e74b97bd6413ed85a959123
Alone4869 <2310523896@qq.com>
2024-08-18 00:52:49 +0800

    新增Post 自定义jsonBody

--- Common/EasyHttp.cs
+++ Common/EasyHttp.cs
@@ -9,81 +9,167 @@ namespace Common
 	/// </summary>
 	public sealed class EasyHttp
 	{
-
-		public static async Task<Dictionary<string,object>> Post(string requestUrl,Dictionary<string,object> headers, Dictionary<string,object> body, string contentType = "application/json")
+		/// <summary>
+		/// HTTP POST 请求
+		/// </summary>
+		/// <param name="requestUrl"></param>
+		/// <param name="headers"></param>
+		/// <param name="body"></param>
+		/// <param name="contentType"></param>
+		/// <returns></returns>
+		public static async Task<Dictionary<string,object>> Post(string requestUrl,Dictionary<string,object> headers, Dictionary<string,object>? body, string contentType = "application/json")
 		{
-			using (var client = new HttpClient())
+			using var client = new HttpClient();
+			
+			var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+
+			if (body != null)
 			{
-				var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
+				request.Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, contentType);
+			}
 
-				if (body != null)
+			foreach (var header in headers.Where(header => !header.Key.Equals("content-type", StringComparison.CurrentCultureIgnoreCase)))
+			{
+				request.Headers.Add(header.Key, header.Value.ToString());
+			}
+
+
+			var response = await client.SendAsync(request);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				var responseBody = await response.Content.ReadAsStringAsync();
+
+				return new Dictionary<string, object>
 				{
-					request.Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, contentType);
-				}
-
-				foreach (var header in headers)
+					{ "code", response.StatusCode },
+					{ "result", responseBody },
+				};
+			}
+			else
+			{
+				return new Dictionary<string, object>()
 				{
-					if (header.Key.ToLower() == "content-type")
-						continue;
-					request.Headers.Add(header.Key, header.Value.ToString());
-				}
-
-
-				var response = await client.SendAsync(request);
-
-				if (response.StatusCode == HttpStatusCode.OK)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
-
-					return new Dictionary<string, object>
-					{
-						{ "code", response.StatusCode },
-						{ "result", responseBody },
-					};
-				}
-				else
-				{
-					return new Dictionary<string, object>()
-							{
-								{ "code", response.StatusCode},
-								{ "result", ""},
-							};
-				}
+					{ "code", response.StatusCode},
+					{ "result", ""},
+				};
 			}
 		}
 
+		/// <summary>
+		/// HTTP POST 请求
+		/// </summary>
+		/// <param name="requestUrl"></param>
+		/// <param name="headers"></param>
+		/// <param name="jsonBody"></param>
+		/// <returns></returns>
+		public static async Task<Dictionary<string,object>> Post(string requestUrl,Dictionary<string,object> headers, string? jsonBody)
+		{
+			using var client = new HttpClient();
+		
+			var request = new HttpRequestMessage(HttpMethod.Post, requestUrl);
 
+			if (jsonBody != null)
+			{
+				request.Content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+			}
+
+			foreach (var header in headers.Where(header => !header.Key.Equals("content-type", StringComparison.CurrentCultureIgnoreCase)))
+			{
+				request.Headers.Add(header.Key, header.Value.ToString());
+			}
+
+
+			var response = await client.SendAsync(request);
+
+			if (response.StatusCode == HttpStatusCode.OK)
+			{
+				var responseBody = await response.Content.ReadAsStringAsync();
+
+				return new Dictionary<string, object>
+				{
+					{ "code", response.StatusCode },
+					{ "result", responseBody },
+				};
+			}
+
+			return new Dictionary<string, object>()
+			{
+				{ "code", response.StatusCode},
+				{ "result", ""},
+			};
+		}
+
+		/// <summary>
+		/// HTTP GET 请求
+		/// </summary>
+		/// <param name="requestUrl"></param>
+		/// <param name="headers"></param>
+		/// <returns></returns>
 		public static async Task<Dictionary<string,object>> Get(string requestUrl, Dictionary<string, object> headers)
 		{
-			using (var client = new HttpClient())
+			using var client = new HttpClient();
+			
+			var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
+			foreach(var header in headers)
 			{
-				var request = new HttpRequestMessage(HttpMethod.Get, requestUrl);
-				foreach(var header in headers)
-				{
-					request.Headers.Add(header.Key, header.Value.ToString());
-				}
+				request.Headers.Add(header.Key, header.Value.ToString());
+			}
 
-				var response = await client.SendAsync(request);
-				if(response.StatusCode == HttpStatusCode.OK)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
+			var response = await client.SendAsync(request);
+			if(response.StatusCode == HttpStatusCode.OK)
+			{
+				var responseBody = await response.Content.ReadAsStringAsync();
 
-					return new Dictionary<string, object>
-					{
-						{ "code", response.StatusCode },
-						{ "result", responseBody },
-					};
-				}
-				else
+				return new Dictionary<string, object>
 				{
-					return new Dictionary<string, object>()
-							{
-								{ "code", response.StatusCode},
-								{ "result", ""},
-							};
-				}
-
+					{ "code", response.StatusCode },
+					{ "result", responseBody },
+				};
+			}
+			else
+			{
+				return new Dictionary<string, object>()
+				{
+					{ "code", response.StatusCode},
+					{ "result", ""},
+				};
 			}
 		}
+		
+		/// <summary>
+		/// 获取重定向链接
+		/// </summary>
+		/// <param name="url"></param>
+		/// <returns></returns>
+		public static async Task<string?> GetRedirectUrl(string url)
+		{
+			try
+			{
+				using var client = new HttpClient();
+				var resp = await client.GetAsync(url);
+				if (resp.RequestMessage == null) return null;
+			
+				var location = "";
+				var referer = "";
+				if (resp.StatusCode == System.Net.HttpStatusCode.Redirect)
+				{
+					location = resp.Headers.Location?.ToString();
+				}
+
+				if (resp.RequestMessage.Headers.Contains("Referer"))
+				{
+					referer = resp.RequestMessage.Headers.GetValues("Referer").FirstOrDefault();
+				}
+				return location ?? referer;
+
+			}
+			catch (Exception ex)
+			{
+				// Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+		
 	}
 }
